737de3c0a21fe2a444452ac29b9acb405de8fa34
Isaac Jarquin <81747286+ijarquin@users.noreply.github.com>
2025-09-01 21:41:19 +0100

    FEATURE add pet cards to adoption pets page (#15)
    * Acceptance Criteria
      When user navigate to adoption pets page, page should display pet cards with:
        -  image carousel
        -  toggle button that display more pet details
        - social share icons hidden, with a button to interact with them
    
     PR: https://github.com/ijarquin/my-lost-pet-rust/pull/15
     ISSUE: https://github.com/ijarquin/my-lost-pet-rust/issues/16

--- src/pages/adoption_pets.rs
+++ src/pages/adoption_pets.rs
@@ -1,14 +1,63 @@
 use dioxus::prelude::*;
 
+use crate::components::Pet;
+use crate::components::PetCard;
+
 #[component]
 pub fn AdoptionPets() -> Element {
+    let pets = vec![
+        Pet {
+            id: 1,
+            name: "Xira",
+            images: vec![
+                asset!("/assets/images/xira.jpg"),
+                asset!("/assets/images/tirma-1.jpg"),
+            ],
+            sex: "Female",
+            age: "2 years",
+            size: "Medium",
+            breed: "Beagle",
+        },
+        Pet {
+            id: 2,
+            name: "Luna",
+            images: vec![asset!("/assets/images/luna.jpg")],
+            sex: "Female",
+            age: "3 years",
+            size: "Small",
+            breed: "Poodle",
+        },
+        Pet {
+            id: 3,
+            name: "Max",
+            images: vec![asset!("/assets/images/alma.jpg")],
+            sex: "Male",
+            age: "4 years",
+            size: "Large",
+            breed: "German Shepherd",
+        },
+        Pet {
+            id: 4,
+            name: "Rudolf",
+            images: vec![asset!("/assets/images/rudolf.jpg")],
+            sex: "Male",
+            age: "5 years",
+            size: "Medium",
+            breed: "Beagle",
+        },
+    ];
+
     rsx! {
         div {
-            id: "adoption-pets",
-            class: "text-center",
-            // Content
-            h1 { "Adoption Pets" }
-            p { "This page is a placeholder for adoption pets. You can navigate to the blog or home page." }
+            id: "missing-pets",
+            class: "missing-pets",
+            {
+                pets.iter().map(|pet| {
+                    rsx! {
+                        PetCard {pet: pet.clone()}
+                    }
+                })
+            }
         }
     }
 }
