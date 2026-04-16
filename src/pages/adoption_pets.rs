ccd360b41fa0eb3974e27913f4822cc226e5f09b
Isaac Jarquin <81747286+ijarquin@users.noreply.github.com>
2026-04-16 22:28:39 +0100

    Feature mobile responsive (#19)
    * feat: make main pages responsive
    
    * feat: make the homepage responsive
    
    * feat: fix the pages layout
    
    * feat: add hamburger button for mobile version of the site
    
    * lint: fix lint errors

--- src/pages/adoption_pets.rs
+++ src/pages/adoption_pets.rs
@@ -5,7 +5,7 @@ use crate::components::PetCard;
 
 #[component]
 pub fn AdoptionPets() -> Element {
-    let pets = vec![
+    let pets = [
         Pet {
             id: 1,
             name: "Xira",
